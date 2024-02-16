b61bd9ef054e0afd3c44473d3291ad4a5104d0e6
somesh mahule <someshmahule@gmail.com>
2024-02-15 23:15:09 -0500

    Added Delete node function and made switch panel for all operations

--- LinkedList/LinkList.cc
+++ LinkedList/LinkList.cc
@@ -33,6 +33,28 @@ void LinkList::insert_node(int data)
     }
 }
 
+
+void LinkList::delete_node(int data)
+{
+    if(head_->data_ == data) //delete head node
+    {
+        Node *tmp = head_;
+        head_ = head_->next_;
+        delete tmp;
+    } 
+    else
+    {
+        Node *start = head_;
+        Node *prev = head_;
+        while(start->data_ != data){
+            prev = start;
+            start = start->next_;
+        }
+        prev->next_ = start->next_;
+        delete start;
+    }
+}
+
 void LinkList::print_list()
 {
     if(head_)
--- LinkedList/LinkList.m.cc
+++ LinkedList/LinkList.m.cc
@@ -7,11 +7,41 @@ int main()
 {
 
     LinkList a;
-    for(int i=1;i<5;i++)
-    {   
-        a.insert_node(i);
+    while (1)
+    {
+        cout<<"Select Operation:"<<endl;
+        cout<<"1. Insert"<<endl;
+        cout<<"2. Delete"<<endl;
+        cout<<"3. Print List"<<endl;
+        cout<<"4. Exit"<<endl;
+        int op;
+        cin>>op;
+        switch (op)
+        {
+        case 1:
+            cout<<"Enter data to insert"<<endl;
+            int data;
+            cin>>data;
+            a.insert_node(data);
+            break;
+        case 2:
+            cout<<"Enter data to delete"<<endl;
+            int del;
+            cin>>del;
+            a.delete_node(del);
+            break;
+        case 3:
+            cout<<"List ->"<<endl;
+            a.print_list();
+            break;
+        case 4:
+            exit(0);
+        default:
+            break;
+        }
+        
     }
-
-    a.print_list();
+    
+    
 
 }
